c5704857f4cb9b118a0b7ef0afa26d191b4485dd
Pranjul Singh <singhpranjul001@gmail.com>
2024-05-01 16:50:14 +0530

    Initial commit

---
+++ src/main/java/com/practice/mvc/Customer.java
@@ -0,0 +1,51 @@
+package com.practice.mvc;
+
+import jakarta.validation.constraints.*;
+
+public class Customer {
+    private String firstName;
+    //adding validation rules
+    @NotNull(message = "Last Name is required")
+    @Size(min = 1, message = "Last Name is required")
+    private String lastName;
+
+    @Min(value=0, message = "Age must be more than 0")
+    @Max(value=120, message = "Age must be less than 120")
+    @NotNull(message ="Age is required")
+    private Integer age;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 chars/digits allowed")
+    private String postalCode;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
